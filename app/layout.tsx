8b8c684fbb7cb746cf8afbc84e1c0f16d691706e
plastic041 <plastic041@gmail.com>
2023-09-08 09:55:32 +0900

    feat: font roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,16 @@ import { Roboto_Slab } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
 
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 
 import type { ReactNode } from "react";
 
-const robotoSlab = Roboto_Slab({ subsets: ["latin"] });
+const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-slab",
+});
 
 export default function App({ children }: { children: ReactNode }) {
   return (
